39a4f93a3d992f6b8a2d8fa6f7703f50b1bbf9f6
Paucinha <paucia.renan@gmail.com>
2023-02-15 23:34:46 -0300

    aquivos finalizados

---
+++ tipos-variaveis/src/OperadoresRelacionais.java
@@ -0,0 +1,32 @@
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2){
+            System.out.println("a nossa consição é verdadeira");
+        }
+	    
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior a numeroDois? " + simNao);
+        
+        simNao = numero1 < numero2;
+
+        System.out.println("numeroUm é menor a numeroDois? " + simNao);
+
+        // ComparacaoAvancada
+        String nomeUm = "PAUCIA";
+        String nomeDois = new String("PAUCIA");
+
+        System.out.println(nomeUm.equals(nomeDois)); //true
+    }
+}
---
+++ tipos-variaveis/src/OperadoresTernario.java
@@ -0,0 +1,11 @@
+public class OperadoresTernario {
+    public static void main(String[] args) {
+        int a, b;
+        a = 6;
+        b = 6;
+
+        int resultado = a==b ? 1 : 0;
+    
+        System.out.println(resultado);   
+    }
+}
